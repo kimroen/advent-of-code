acd3d3c9dda067ffb50625178b470131965b2f03
Kim Røen <kim@kimroen.com>
2016-12-04 21:53:49 +0100

    Create new class for parsing the directions

---
+++ 1/lib/direction_parser.rb
@@ -0,0 +1,2 @@
+class DirectionParser
+end
---
+++ 1/test/direction_parser_test.rb
@@ -0,0 +1,9 @@
+gem 'minitest', '>=5'
+
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require 'direction_parser'
+
+class DirectionParserTest < Minitest::Test
+end
