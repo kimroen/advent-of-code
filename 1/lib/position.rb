5e454308f83def6af345c79a568725d982e11a66
Kim Røen <kim@kimroen.com>
2016-12-04 16:34:41 +0100

    Allow position to walk north

--- 1/lib/position.rb
+++ 1/lib/position.rb
@@ -1,12 +1,20 @@
 class Position
   attr_reader :x, :y
 
-  def initialize
-    @x = 0
-    @y = 0
+  def initialize(x: 0, y: 0)
+    @x = x
+    @y = y
   end
 
   def coordinates
     { x: x, y: y }
   end
+
+  def walk(_direction)
+    self.y += 1
+  end
+
+  private
+
+  attr_writer :x, :y
 end
--- 1/test/position_test.rb
+++ 1/test/position_test.rb
@@ -8,10 +8,19 @@ require 'position'
 class PositionTest < Minitest::Test
   def test_it_has_coordinates
     position = Position.new
-    expected_coordinates = { x: 0, y: 0 }
 
+    expected_coordinates = { x: 0, y: 0 }
     assert_equal expected_coordinates, position.coordinates
     assert_equal 0, position.y
     assert_equal 0, position.x
+  end
+
+  def test_it_can_walk_north
+    position = Position.new
+
+    position.walk(:north)
+
+    expected_coordinates = { x: 0, y: 1 }
+    assert_equal expected_coordinates, position.coordinates
   end
 end
